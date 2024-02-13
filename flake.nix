522d32319d9805b265be4489235cf818524a1a34
Gautier DI FOLCO <gautier.difolco@gmail.com>
2024-02-13 19:45:39 +0100

    Prepare for WASM

--- flake.nix
+++ flake.nix
@@ -4,16 +4,35 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
   flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
-    pkgs = import nixpkgs {inherit system;};
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [inputs.rust-overlay.overlays.rust-overlay];
+    };
   in rec {
     devShell = pkgs.mkShell {
       CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
 
-      buildInputs = with pkgs; [ cargo rustc rust-analyzer git ];
+      buildInputs = with pkgs; [
+        # cargo
+        # rustc
+        # rust-analyzer
+        git
+        trunk
+        cargo-binutils
+        lld
+        wasm-pack
+        wasm-bindgen-cli
+        (rust-bin.fromRustupToolchain {
+              channel = "stable";
+              components = ["rust-analyzer" "rust-src" "rustfmt" "rustc" "cargo"];
+              targets = ["wasm32-unknown-unknown"];
+        })
+      ];
     };
   });
 }
